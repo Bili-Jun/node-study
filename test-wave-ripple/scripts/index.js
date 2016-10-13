5da07ee5dfabe3435bf0d9d0a8f6e24a5c77d956
7274 <7274@rdc.hand-china.com>
2016-10-13 17:57:44 +0800

    add some new

--- test-wave-ripple/scripts/index.js
+++ test-wave-ripple/scripts/index.js
@@ -1,61 +1,443 @@
-window.onload = function() {
-    console.log("test");
+﻿class Metcha {
+
+    static convertStyle(object) {
+        var style = '';
+
+        for (var a in object) {
+            if (object.hasOwnProperty(a)) {
+                style += (a + ':' + object[a] + ';');
+            }
+        }
+
+        return style;
+    }
 }
 
-;
-(function(window) {
-    "use script" 
-
-    var waves = waves || {},
-        el = document.querySelectorAll.bind(document);
-
-    
-
-    waves.startEvent = function() {
-        document.body.addEventListener("mousedown", function(e) {
-            //console.log(e.clientX, e.clientY /*this.documentElement .clientTop, this.documentElement.clientLeft*/ );
-            var ripple = document.createElement("div"),
-                elem = getWaveElement(e);
-            ripple.className = "waves-ripple";
-            
-            elem.appendChild(ripple);
-
-            var docElem, win,
-            box = {top: 0, left: 0},
-            doc = elem && elem.ownerDocument;
-
-        docElem = doc.documentElement;
-
-        if (typeof elem.getBoundingClientRect !== typeof undefined) {
-            box = elem.getBoundingClientRect();
-        }
-        win = getWindow(doc);
-        var pos={
-                top: box.top + win.pageYOffset - docElem.clientTop,
-                left: box.left + win.pageXOffset - docElem.clientLeft
-            }
-            
-            console.log(e,pos.top);
-            ripple.setAttribute("style","top:"+(e.pageY-pos.top)
-            +"px;left:"+(e.pageX-pos.left)+"px;");
-        })
-        
+String.prototype.isEmpty = function () {
+    let flag = true;
+    if (this.replace(/(^\s*)|(\s*$)/g, "").length > 0) {
+        flag = false;
     }
-    function getWaveElement(e){
-        var element = null;
-        element = e.target || e.srcElement;
+    return flag;
+}
+String.prototype.isNotEmpty = function (flag = false) {
+    flag = this.toString().isEmpty();
+    return !flag;
+};
+
+
+(function (window) {
+    'use script'
+    // var [waves, el, event, isWindow, getWindow,disable] = [
+    //     waves || {},
+    //     document.querySelectorAll.bind(document),
+    //     event || {},
+    //     obj => obj !== null && obj === obj.window,
+    //     element => isWindow(element) ? element : element.nodeType === 9 && element.defaultView
+    // ];
+
+    const [
+        waves,
+        event,
+        el,
+        isWindow,
+        getWindow,
+        disableRightButton
+    ] = [
+            //waves:初始化waves
+            {},
+
+            //event:wave基本事件
+            {
+                duration: 750
+            },
+
+            //通过绑定document,获取document内的元素
+            document.querySelectorAll.bind(document),
+
+            //判断对象是否是window对象
+            obj => obj !== null && obj === obj.window,
+
+            //获取window对象
+            element => isWindow(element) ? element : (element.nodeType === 9 && element.defaultView),
+
+            //禁用鼠标右键点击事件
+            e => e.button && e.button === 2 ? false : true
+        ];
+
+    /*获取className 含有'waves'的元素*/
+    function getWavesEventElement(e) {
+        let [element, target] = [null, null];
+        if (e != undefined) {
+            target = e.target || e.srcElement;
+            while (target.parentElement != null) {
+                if (target.className.indexOf('waves') !== -1) {
+                    element = target;
+                    break;
+                } else if (target.classList.contains('waves')) {
+                    element = target;
+                    break;
+                }
+                target = target.parentElement;
+            }
+        }
+
+
         return element;
     }
-    function isWindow(obj) {
-        return obj !== null && obj === obj.window;
+
+    //Object to css string
+    function convertStyle(obj) {
+        var style = '';
+
+        for (var a in obj) {
+            if (obj.hasOwnProperty(a)) {
+                style += (a + ':' + obj[a] + ';');
+            }
+        }
+
+        return style;
     }
 
-    function getWindow(elem) {
-        return isWindow(elem) ? elem : elem.nodeType === 9 && elem.defaultView;
+
+    //waves 基本事件:显示ripple
+    event.show = function (e, element) {
+
+        disableRightButton(e);
+
+        let [
+            ripple,
+            waveElem,
+            basePos,
+            baseDoc,
+            docElem,
+            win,
+            rippleStyle,
+            scale,
+            relativeY,
+            relativeX
+        ] = [
+                //创建riiple div
+                document.createElement('div'),
+
+                //初始化class 含有'waves'的元素
+                (element || this),
+
+                //初始化元素相对位置
+                {
+                    top: 0,
+                    left: 0
+                },
+
+                //初始化document元素
+                new Object,
+
+                //初始化documentElement
+                new Object,
+
+                //初始化window对象
+                new Object,
+
+                //初始化riiple 样式
+                {},
+
+                //css scale属性
+                null,
+
+                //鼠标实时点击元素的相对位置:Y轴
+                0,
+
+                //鼠标实时点击元素的相对位置:X轴
+                0,
+            ];
+
+        //设置ripple className属性    
+        ripple.className = "waves-ripple";
+
+        //将riiple插入waves element
+        waveElem.appendChild(ripple);
+
+        //设置document 对象
+        baseDoc = waveElem && waveElem.ownerDocument;
+
+        //设置documentElement
+        docElem = baseDoc.documentElement;
+
+        //获取并设置window对象
+        win = getWindow(baseDoc);
+
+        //绑定鼠标实时点击 clientRect
+        if (typeof waveElem.getBoundingClientRect !== undefined) {
+            basePos = waveElem.getBoundingClientRect();
+        }
+
+        //设置鼠标实时点击元素的相对位置:Y轴
+        relativeY = e.pageY - (basePos.top + win.pageYOffset - docElem.clientTop);
+
+        //设置鼠标实时点击元素的相对位置:X轴
+        relativeX = e.pageX - (basePos.left + win.pageXOffset - docElem.clientLeft);
+
+        scale = 'scale(' + ((waveElem.clientWidth / 100) * 10) + ')';
+
+        rippleStyle = {
+            'top': relativeY + 'px',
+            'left': relativeX + 'px'
+        }
+
+        //设置ripple 动态css样式
+        ripple.setAttribute('style', convertStyle(rippleStyle));
+
+        ripple.setAttribute('data-start', Date.now());
+        ripple.setAttribute('data-scale', scale);
+        ripple.setAttribute('data-x', relativeY);
+        ripple.setAttribute('data-y', relativeX);
+
+        rippleStyle['-webkit-transform'] = scale;
+        rippleStyle['-moz-transform'] = scale;
+        rippleStyle['-ms-transform'] = scale;
+        rippleStyle['-o-transform'] = scale;
+        rippleStyle.transform = scale;
+        rippleStyle.opacity = '1';
+
+        rippleStyle['-webkit-transition-duration'] = 750 + 'ms';
+        rippleStyle['-moz-transition-duration'] = 750 + 'ms';
+        rippleStyle['-o-transition-duration'] = 750 + 'ms';
+        rippleStyle['transition-duration'] = 750 + 'ms';
+
+        rippleStyle['-webkit-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+        rippleStyle['-moz-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+        rippleStyle['-o-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+        rippleStyle['transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+
+        ripple.setAttribute('style', convertStyle(rippleStyle));
+
     }
 
-    document.addEventListener("DOMContentLoaded", function() {
-        //var ele=el("div");
-        waves.startEvent();
+    event.hide = function (e) {
+        //当前指针
+        let $this = this;
+
+        let [
+            ripple,
+            ripples,
+            waveElem,
+            delay,
+            relativeX,
+            relativeY,
+            scale
+        ] = [
+                new Object,
+                //获取ripples元素
+                $this.getElementsByClassName('waves-ripple'),
+
+                //获取waves 元素
+                getWavesEventElement(e),
+
+                //ripple 延迟消失时间初始化
+                0,
+
+                //设置鼠标实时点击元素的相对位置:Y轴
+                0,
+
+                //设置鼠标实时点击元素的相对位置:X轴
+                0,
+            ]
+        if (ripples.length > 0) {
+            ripple = ripples[ripples.length - 1]
+        } else {
+            return false;
+        }
+
+        relativeY = ripple.getAttribute('data-y');
+        relativeX = ripple.getAttribute('data-x');
+        scale = ripple.getAttribute('data-scale');
+
+        delay = 350 - (Date.now() - Number(ripple.getAttribute('data-start')));
+
+        if (delay < 0) {
+            delay = 0;
+        }
+
+        setTimeout(function () {
+            let rippleStyle = {
+                'top': relativeY + 'px',
+                'left': relativeX + 'px',
+                'opacity': '0',
+
+                // Duration
+                '-webkit-transition-duration': event.duration + 'ms',
+                '-moz-transition-duration': event.duration + 'ms',
+                '-o-transition-duration': event.duration + 'ms',
+                'transition-duration': event.duration + 'ms',
+                '-webkit-transform': scale,
+                '-moz-transform': scale,
+                '-ms-transform': scale,
+                '-o-transform': scale,
+                'transform': scale,
+            }
+
+            ripple.setAttribute('style', convertStyle(rippleStyle));
+
+            setTimeout(function () {
+                try {
+                    $this.removeChild(ripple);
+                } catch (e) {
+                    return false;
+                }
+            }, event.duration);
+        }, delay);
+        console.log(delay);
+    }
+
+    waves.effect = function (e) {
+        let element = getWavesEventElement(e);
+        if (element != null) {
+            event.show(e, element);
+            element.addEventListener("mouseup", event.hide, false);
+            element.addEventListener("mouseleave", event.hide, false);
+        }
+
+    }
+
+    waves.run = function () {
+        document.body.addEventListener("mousedown", waves.effect, false);
+
+    }
+
+    window.waves = waves;
+
+    document.addEventListener('DOMContentLoaded', function () {
+        waves.run();
+
     }, false);
+
+
+
+
+
 })(window);
+
+// ; (function (window) {
+//     "use script"
+
+//     var waves = waves || {},
+//         el = document.querySelectorAll.bind(document);
+
+
+
+//     waves.startEvent = function (e) {
+
+//             //console.log(e.clientX, e.clientY /*this.documentElement .clientTop, this.documentElement.clientLeft*/ );
+//             var ripple = document.createElement("div"),
+//                 elem = getWaveElement(e);
+//             ripple.className = "waves-ripple";
+//             if (elem.className.isNotEmpty() && (elem.className).indexOf("waves") > -1) {
+//                 elem.appendChild(ripple);
+
+//                 var docElem, win,
+//                     box = { top: 0, left: 0 },
+//                     doc = elem && elem.ownerDocument;
+
+//                 docElem = doc.documentElement;
+
+//                 if (typeof elem.getBoundingClientRect !== typeof undefined) {
+//                     box = elem.getBoundingClientRect();
+//                 }
+//                 win = getWindow(doc);
+//                 var scale       = 'scale('+((elem.clientWidth / 100) * 10)+')';
+//                 var pos = {
+//                     top: box.top + win.pageYOffset - docElem.clientTop,
+//                     left: box.left + win.pageXOffset - docElem.clientLeft
+//                 }
+
+//                 var relativeY   = (e.pageY - pos.top);
+//                 var relativeX   = (e.pageX - pos.left);
+
+//                 //console.log(e, rippleStyle.top);
+//                /* ripple.setAttribute("style", "top:" + relativeY
+//                     + "px;left:" + relativeX + "px;");*/
+
+//                 var rippleStyle = {
+//                     'top': relativeY+'px',
+//                     'left': relativeX+'px'
+//                 };
+
+//                 ripple.setAttribute('data-hold', Date.now());
+//                 ripple.setAttribute('data-scale', scale);
+//                 ripple.setAttribute('data-x', relativeY.top);
+//                 ripple.setAttribute('data-y', relativeX.left);
+
+//                 rippleStyle['-webkit-transform'] = scale;
+//                 rippleStyle['-moz-transform'] = scale;
+//                 rippleStyle['-ms-transform'] = scale;
+//                 rippleStyle['-o-transform'] = scale;
+//                 rippleStyle.transform = scale;
+//                 rippleStyle.opacity = '1';
+
+//                 rippleStyle['-webkit-transition-duration'] = 750 + 'ms';
+//                 rippleStyle['-moz-transition-duration'] = 750 + 'ms';
+//                 rippleStyle['-o-transition-duration'] = 750 + 'ms';
+//                 rippleStyle['transition-duration'] = 750 + 'ms';
+
+//                 rippleStyle['-webkit-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+//                 rippleStyle['-moz-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+//                 rippleStyle['-o-transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+//                 rippleStyle['transition-timing-function'] = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
+
+//                 convertStyle(rippleStyle);
+//                 ripple.setAttribute('style', convertStyle(rippleStyle));
+//                 console.log(elem.className);
+//             }
+
+
+
+
+//     }
+
+//     waves.stopEvent = function(e){
+//         console.log(e);
+//     } 
+//     function getWaveElement(e) {
+//         var element = null;
+//         element = e.target || e.srcElement;
+//         return element;
+//     }
+//     function isWindow(obj) {
+//         return obj !== null && obj === obj.window;
+//     }
+
+//     function getWindow(elem) {
+//         return isWindow(elem) ? elem : elem.nodeType === 9 && elem.defaultView;
+//     }
+
+//     function convertStyle(obj) {
+//         var style = '';
+
+//         for (var a in obj) {
+//             if (obj.hasOwnProperty(a)) {
+//                 style += (a + ':' + obj[a] + ';');
+//             }
+//         }
+
+//         return style;
+//     }
+//     waves.attach = function(element){
+//         waves.startEvent(element);
+//     }
+//     window.waves=waves;
+
+
+//     document.addEventListener("DOMContentLoaded", function () {
+//         //var ele=el("div");
+//         document.body.addEventListener("mousedown", function (e) {
+//             //waves.startEvent(e);
+//         });
+//         document.body.addEventListener("mouseup", function (e) {
+//             //waves.stopEvent(e);
+//         });
+//         document.body.addEventListener("mouseleave", function (e) {
+//             //waves.stopEvent(e);
+//         });
+
+//     }, false);
+// })(window);
