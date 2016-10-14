d44cc0f972396b9a87ca59724c31df9c8a8ccba7
7274 <7274@rdc.hand-china.com>
2016-10-14 16:43:27 +0800

    add gulp

---
+++ test-wave-ripple/gulpfile.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const gulp = require('gulp');
+const webpackConfig = require("./webpack.config.js");
+const eslint = require('eslint');
+gulp.task('default', ['webpack','watch']);
+
+gulp.task('webpack', function(callback) {
+  var myConfig = Object.create(webpackConfig);
+    // run webpack
+    webpack(
+        // configuration
+        myConfig
+    , function(err, stats) {
+        // if(err) throw new gutil.PluginError("webpack", err);
+        // gutil.log("[webpack]", stats.toString({
+        //     // output options
+        // }));
+        callback();
+    });
+});
+
+gulp.task('watch', function () {
+   gulp.watch('scripts/*.js', ['webpack']);
+   
+});
+
