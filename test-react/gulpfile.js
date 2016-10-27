e475df7aa23208923346602d7afce7185265abbc
7274 <7274@rdc.hand-china.com>
2016-10-27 17:55:23 +0800

    update

--- test-react/gulpfile.js
+++ test-react/gulpfile.js
@@ -1,10 +1,11 @@
 const _ = require('lodash');
 const webpack = require('webpack');
 const gulp = require('gulp');
+const uglify = require('gulp-uglify');
 const webpackConfig = require('./webpack.config.js');
 
 
-gulp.task('default', ['webpack', 'watch']);
+gulp.task('default', ['build', 'webpack', 'watch']);
 
 gulp.task('webpack', (callback) => {
   webpack(_.merge(webpackConfig, {
@@ -17,4 +18,10 @@ gulp.task('webpack', (callback) => {
 
 gulp.task('watch', () => {
   gulp.watch('src/*.js', ['webpack']);
+});
+
+gulp.task('build', () => {
+  gulp.src('src/**/*.js')
+         .pipe(uglify())
+         .pipe(gulp.dest('dist'));
 });
